2cf4c083a4c892f061c69c118ec6e3f37b374feb
JozefBucany <jbucany@gmail.com>
2026-07-15 17:23:57 +0200

    cely pohyb aj so spiatockou

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x: float, y:float):
@@ -19,11 +19,24 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += dt * PLAYER_TURN_SPEED
 
+    def move(self, dt):
+        self.speed = PLAYER_SPEED
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
+
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        reverse = -1 if keys[pygame.K_s] else 1
 
-            self.rotate(-dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt * reverse)
         if keys[pygame.K_d]:
-            self.rotate(dt)
+            self.rotate(dt * reverse)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
